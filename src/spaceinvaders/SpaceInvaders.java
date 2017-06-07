2511838b8c3057eb0c2f3eab1499474fc8e1b9c2
Lovejit Kharod <kharodl@gmail.com>
2017-06-07 14:22:22 -0700

    Space Invaders catch up

--- src/spaceinvaders/SpaceInvaders.java
+++ src/spaceinvaders/SpaceInvaders.java
@@ -11,11 +11,12 @@ public class SpaceInvaders extends GameRootPane {
 	PixelSprite invader1;
 	PixelSprite invader2;
 	PixelSprite bullet;
-	Score currentScore = new Score(this, 20, Color.WHITE, Color.TRANSPARENT, Pos.TOP_CENTER);
+	
+	Score score;
 	
 	public SpaceInvaders () 
 	{
-		super("Space Invaders","pixel.ttf","SpaceInvadersTheme.mp3",50,5);
+		super("Space\nInvaders","press-start.ttf","SpaceInvadersTheme.mp3",50,5);
 		
 		String tutorialText = "test"+
 							  "test2";
@@ -60,12 +61,13 @@ public class SpaceInvaders extends GameRootPane {
 			public KeyCode getKey() {return KeyCode.R;}
 			public boolean fireOnce() {return false;}
 			public void action () {
-				ship.moveTo(0,0);
+				ship.moveTo(300,600);
 			}
 		});
 	}
 
 	public void onGameStart() {
+		score = new Score(this, 20, Color.WHITE, Color.TRANSPARENT, Pos.TOP_CENTER);
 		setBackground(Color.BLACK);
 		int[][] spriteShip = new int [][]{{0,0,0,0,0,0,1,0,0,0,0,0,0},
 										  {0,0,0,0,0,1,1,1,0,0,0,0,0},
@@ -88,17 +90,23 @@ public class SpaceInvaders extends GameRootPane {
 											  {1,1,1,1,1,1,1,1},
 											  {0,0,1,0,0,1,0,0},
 											  {0,1,0,1,1,0,1,0},
-											  {1,0,1,0,0,1,0,1}};
+											  {1,0,1,0,0,1,0,1}};		
+		int[][] spriteBullet = new int [][]{{0,1,0},
+										   {1,1,1},
+										   {1,1,1},
+										   {1,1,1},
+										   {1,1,1}};
 									  		  
 		ship = new PixelSprite(spriteShip,50,50,Color.WHITE);
+		bullet = new PixelSprite(spriteBullet,50,50,Color.WHITE);
 		invader1 = new PixelSprite(spriteInvader1,50,50,"enemy",Color.WHITE);
 		invader2 = new PixelSprite(spriteInvader2,50,50,"enemy",Color.WHITE);
 		
-		this.addSprite(ship,100,100);
+		this.addSprite(ship,100,600);
 		for (int x = 10; x <= 400; x += 50)
 		{
-			this.addSprite(invader1, x, 600);
-			this.addSprite(invader2, x, 400);
+			this.addSprite(invader1, x, 100);
+			this.addSprite(invader2, x, 200);
 		}
 		//test2.moveTo(100, 100);
 		//test2.translateAnimation(0, 100, 1000, true, false);
@@ -108,7 +116,7 @@ public class SpaceInvaders extends GameRootPane {
 	public void update() {
 		if (bullet.collided("enemy")) {
 			//removeSprite(bullet.getCollided("enemy"));
-			currentScore.addToScore(10);
+			score.addToScore(10);
 		}
 	}
 	
